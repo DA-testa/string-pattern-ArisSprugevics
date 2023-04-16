409b9548a72b2c8952055043174a98126681f219
ArisSprugevics <aris.sprugevics@edu.rtu.lv>
2023-04-16 23:42:49 +0300

    iespejams

--- hash_substring.py
+++ hash_substring.py
@@ -4,13 +4,14 @@ def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     input_type = input()
-    print(input_type)
     if input_type == 'I':
-        pattern = input()
-        text = input()
+        pattern = input().rstrip()
+        text = input().rstrip()
     else:
-        fileName = input()
-        print(fileName)
+        fileName = "tests/06"
+        file1 = open(fileName)
+        pattern = file1.readline().rstrip()
+        text = file1.readline().rstrip()
     
     # after input type choice
     # read two lines 
@@ -20,7 +21,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    #return (input().rstrip(), input().rstrip())
+    return (pattern, text)
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -28,13 +29,30 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
+    patternHash = 0
+    patternLength = len(pattern)
+    textHash = 0
+    Q = 101
+    #textHash = hash(text[:patternLength])
+    for i in range(patternLength):
+        patternHash = (10*patternHash+ord(pattern[i])) % Q
+        textHash = (10*textHash+ord(text[i])) % Q
+    occurances = []
+    print("pattern hash:"+ str(patternHash))
+    print("initial text hash"+ str(textHash))
+    for i in range(len(text) - patternLength+1):
+        if patternHash == textHash and text[i:i+patternLength] == pattern:
+            occurances.append(i)
+        if i < len(text) - patternLength:
+            #textHash = (textHash - (ord(text[i])*((10**patternLength-1)%Q)))*10 + ord(text[i+patternLength]) % Q
+            textHash = (10*(textHash - ord(text[i]) * pow(10, patternLength-1, Q)) + ord(text[i+patternLength])) % Q
+            print("new text hash:"+ str(textHash))
 
-    # and return an iterable variable
-    return [0]
+    # and return an iterable variable #[0]
+    return occurances
 
 
 # this part launches the functions
 if __name__ == '__main__':
-    read_input()
-    #print_occurrences(get_occurrences(*read_input()))
+    print_occurrences(get_occurrences(*read_input()))
 
